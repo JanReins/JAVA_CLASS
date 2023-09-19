be5191f514e6314e01b5ef7be11aae40a24249ae
JanReins <140710185+JanReins@users.noreply.github.com>
2023-09-19 13:14:27 +0800

    09.19.2023

--- Assignments/tuition.java
+++ Assignments/tuition.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
-public class tuition {
+public class Tuition {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int unit_enrolled, unit_fee, blanket_fee, total_fee;
-        double miscellaneous_fee;
+        double unit_enrolled, unit_fee, total_fee;
 
         System.out.println("Enter number of unit enrolled: ");
         unit_enrolled = input.nextInt();
 
         unit_fee = 45;
-        miscellaneous_fee = 1.15;
-        blanket_fee = 200;
-
-        total_fee = (((unit_enrolled * unit_fee) * miscellaneous_fee) + blanket_fee);
+        total_fee = (((unit_enrolled * unit_fee) * 1.15 ) + 200);
         System.out.println(total_fee);
 
 
