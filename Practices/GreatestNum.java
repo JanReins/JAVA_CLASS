bced241a8a62023210f5c846b670aa6416f55705
JanReins <140710185+JanReins@users.noreply.github.com>
2023-10-20 17:07:42 +0800

    10.20.2023

--- Practices/GreatestNum.java
+++ Practices/GreatestNum.java
@@ -4,16 +4,14 @@ public class GreatestNum {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        
         System.out.print("Input the 1st number: ");
         int num1 = input.nextInt();
-   
+
         System.out.print("Input the 2nd number: ");
         int num2 = input.nextInt();
-        
+
         System.out.print("Input the 3rd number: ");
         int num3 = input.nextInt();
-
 
         int greatestNumber = num1;
         if (num2 > greatestNumber) {
