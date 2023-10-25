25eb5386ea99d29859eac1423603a41ede5ea271
JanReins <140710185+JanReins@users.noreply.github.com>
2023-10-25 17:23:48 +0800

    10.25.2023

---
+++ Practices/array1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class array1 {
+    public static void main(String[] args) {
+        // try to make an array
+        String[] newArr = new String[5];
+        newArr[0] = "hello";
+        newArr[1] = "hi";
+        newArr[2] = "tim";
+        newArr[3] = "bill";
+        newArr[4] = "joe";
+
+        String x = newArr[4];
+        System.out.println(x);
+
+    }
+}
