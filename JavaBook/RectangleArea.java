2f68234f33d26a0e49b9dcb44fc408ab1c591473
JanReins <dumamagjanreinhold@gmail.com>
2023-10-27 06:32:35 +0800

    10.27.2023

--- JavaBook/RectangleArea.java
+++ JavaBook/RectangleArea.java
@@ -7,4 +7,22 @@ class RectangleArea {
         length = l;
     }
 
+    double AreaCal() {
+        return width * length;
+
+    }
+
 }
+
+class Rectangle1 {
+    public static void main(String[] args) {
+        RectangleArea myrect1 = new RectangleArea(10, 30);
+        double area;
+
+        area = myrect1.AreaCal();
+        System.out.println(area);
+    }
+
+    
+}
+
