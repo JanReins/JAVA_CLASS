47942f5573fb528f2ee091a9721118826d48fcc2
JanReins <dumamagjanreinhold@gmail.com>
2023-11-19 20:36:08 +0800

    11.19.2023

---
+++ Inheritance/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    private int idno;
+    private String name;
+    private int age;
+    private String addres;
+
+    public Person() {
+        pulic Person(int idno, String name, int age, S
+
+    }
+    
+}
